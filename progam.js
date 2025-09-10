877af1df9990d196129d0bd15499ae612c9f38d7
Juliana Florido <jafloridom@udistrital.edu.co>
2025-09-10 15:59:11 -0500

    vr 0.2

--- progam.js
+++ progam.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([4.687640387032661, -74.14950733915333], 13);
+var map = L.map('map').setView([4.715637638989299, -73.96679469812686], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([4.687640387032661, -74.14950733915333]).addTo(map);
+var marker = L.marker([4.715637638989299, -73.96679469812686]).addTo(map);
 var marker = L.marker([4.698932493528999, -74.11327818455958]).addTo(map);
